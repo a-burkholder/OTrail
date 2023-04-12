4735e14b331cd196f8395f44d3fe202f2edac16c
k-mcclure.1 <k-mcclure.1@onu.edu>
2023-04-12 10:46:45 -0400

    finally got the elevation figured out

--- app/src/main/java/com/example/OTrail/MainActivity.java
+++ app/src/main/java/com/example/OTrail/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.OTrail;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +28,12 @@ public class MainActivity extends AppCompatActivity {
                 toTrail.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        // Player is able to set Hattie's family members/pet names.
                         setContentView(R.layout.name_input);
+                        //Display blurb about starting cash, what they could buy, and what they should buy.  (maybe add lore here based on class)
+                        //Display graphic for "Beginning the journey"
+                        //The main loop of the Oregon Trail game. Only runs if the player is not yet at the first fort (250 miles away) and their game is not yet over.
+                            //MAIN LOOP
                     }
                 });
                 toOTrailInfo.setOnClickListener(new View.OnClickListener() {
@@ -35,9 +43,17 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 toCattieInfo.setOnClickListener(new View.OnClickListener() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onClick(View view) {
                         setContentView(R.layout.cattie_info);
+                        final TextView hattietext = findViewById(R.id.hattieinfo);
+                        hattietext.setText("@strings/hattie_info1");
+                        //then wait for user to ask for next box.
+                        hattietext.setText("@strings/hattie_info2");
+                        //then wait for user to ask for next box.
+                        hattietext.setText("@strings/hattie_info3");
+                        //then wait for user to ask for return to menu.
                     }
                 });
                 top10.setOnClickListener(new View.OnClickListener() {
