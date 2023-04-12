93decbfff12028ed37e165f43f3e7717c35f7f72
unit13524 <a-burkholder.2@onu.edu>
2023-04-11 21:39:55 -0400

    Added layout

--- app/src/main/java/com/example/OTrail/MainActivity.java
+++ app/src/main/java/com/example/OTrail/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.OTrail;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.titlescreen);
+        final Button start = findViewById(R.id.titleStart);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
