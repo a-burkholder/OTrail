eb0effd36ba0624e49cee00852ae55178d3b86be
unit13524 <a-burkholder.2@onu.edu>
2023-04-11 22:17:12 -0400

    Main menu selection

--- app/src/main/java/com/example/OTrail/MainActivity.java
+++ app/src/main/java/com/example/OTrail/MainActivity.java
@@ -15,8 +15,54 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                setContentView(R.layout.titlescreen2);
+                final Button toTrail = findViewById(R.id.title1);
+                final Button toOTrailInfo = findViewById(R.id.title2);
+                final Button toCattieInfo = findViewById(R.id.title3);
+                final Button top10 = findViewById(R.id.title4);
+                final Button graves = findViewById(R.id.title5);
+                final Button options = findViewById(R.id.title6);
+                toTrail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.name_input);
+                    }
+                });
+                toOTrailInfo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.trail_info);
+                    }
+                });
+                toCattieInfo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.cattie_info);
+                    }
+                });
+                top10.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.top_10);
+                    }
+                });
+                graves.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.graveyard);
+                    }
+                });
+                options.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.options);
+                    }
+                });
             }
         });
     }
+
+
+
+
 }
